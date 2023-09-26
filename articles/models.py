ba47a3f72d8597a07b56f520bd92304bf15a63ad
gechderib <gechderib@gmail.com>
2023-09-26 11:57:18 +0300

    django model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateField(auto_now_add=True)
+
