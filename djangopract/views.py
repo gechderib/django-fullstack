41300296fc916a3ec2e40463eeb2d2ba3538938c
gechderib <gechderib@gmail.com>
2023-09-26 11:02:12 +0300

    adding templates to django

--- djangopract/views.py
+++ djangopract/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def about(request):
-    return HttpResponse("about")
+    # return HttpResponse("About")
+    return render(request, "aboutpage.html")
 
 def home(request):
-    return HttpResponse("Home")
+    # return HttpResponse("Home")
+    return render(request,"homepage.html")
