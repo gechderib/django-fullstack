3f42bb4e9eda0a6f4c60cc3b281ed74ed31c9b55
gechderib <gechderib@gmail.com>
2023-09-26 16:17:21 +0300

    static files and image in django

--- djangopract/urls.py
+++ djangopract/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("articles/", include("articles.urls")),
     path('about/', views.about),
     path('',views.home)
 ]
+
+urlpatterns += staticfiles_urlpatterns() # check i we are in debug mode and serve the static file
