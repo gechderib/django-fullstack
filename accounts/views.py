209ac79f7c18a636570747bbcd69838e50da3668
gechderib <gechderib@gmail.com>
2023-09-29 10:29:37 +0300

    user creation form djanog

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
     # return HttpResponse("signup user ...")
-    return render(request, "accounts/signup.html")
+    form = UserCreationForm()
+    return render(request, "accounts/signup.html",{"form":form})
