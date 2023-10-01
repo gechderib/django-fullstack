4730ad81ed6a343fd27cb7ff943f697083e3c0d4
gechderib <gechderib@gmail.com>
2023-10-01 20:43:55 +0300

    model

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=50)
@@ -7,6 +7,7 @@ class Article(models.Model):
     slug = models.SlugField()
     date = models.DateField(auto_now_add=True)
     thumbnail = models.ImageField(default="default.png", blank=True)
+    author = models.ForeignKey(User, default=None,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
